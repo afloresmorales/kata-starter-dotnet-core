0cb0c0f60deb80108c613bf9a413aec077bb165b
csolorzanof <carlos.solorzano@acklenavenue.com>
2019-08-28 13:49:14 -0600

    negatives passing, multiple negatives failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -18,6 +18,15 @@ namespace Kata
                 s = str.Last(); // 1;2;3
             }
             var numbers = s.Split(separator, StringSplitOptions.None).Select(int.Parse);
+
+            var negatives = numbers.Where(x => x < 0);
+
+            if (negatives.Any())
+            {
+                throw new Exception($@"negatives not allowed: {negatives.First()}");
+            }
+
+
             if (numbers.Count() == 1)
                 return numbers.First();
 
